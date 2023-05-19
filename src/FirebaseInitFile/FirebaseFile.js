7033bbf1df0c72b3ffd62d83e8107f1d8ac39939
mahfuz012 <mahfuzrn012@gmail.com>
2023-05-19 19:38:04 +0600

    Environment variable add

--- src/FirebaseInitFile/FirebaseFile.js
+++ src/FirebaseInitFile/FirebaseFile.js
@@ -1,9 +1,9 @@
 
 import { initializeApp } from "firebase/app";
+
+
 const firebaseConfig = {
-
-
-  apiKey:import.meta.env.VITE_APIKEY,
+ apiKey:import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
   projectId:import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
