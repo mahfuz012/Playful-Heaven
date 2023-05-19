7dc4730e8e3c7f892f881f4f212d50feb7f8e193
mahfuz012 <mahfuzrn012@gmail.com>
2023-05-19 19:10:58 +0600

    Image add on gallery

--- src/FirebaseInitFile/FirebaseFile.js
+++ src/FirebaseInitFile/FirebaseFile.js
@@ -1,6 +1,5 @@
 
 import { initializeApp } from "firebase/app";
-
 const firebaseConfig = {
   apiKey: "AIzaSyAR-Ykd1VcIG-q0RcvgLrN23FjIXHQ38Gs",
   authDomain: "playful-heaven.firebaseapp.com",
@@ -10,6 +9,5 @@ const firebaseConfig = {
   appId: "1:229837698522:web:ddb9db6faf98f83d027ce3"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
