80681c9200e31b3f535333a72849554d45296f03
mahfuz012 <mahfuzrn012@gmail.com>
2023-05-19 19:10:28 +0600

    Gallery setup

--- src/FirebaseInitFile/FirebaseFile.js
+++ src/FirebaseInitFile/FirebaseFile.js
@@ -1,1 +1,15 @@
 
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAR-Ykd1VcIG-q0RcvgLrN23FjIXHQ38Gs",
+  authDomain: "playful-heaven.firebaseapp.com",
+  projectId: "playful-heaven",
+  storageBucket: "playful-heaven.appspot.com",
+  messagingSenderId: "229837698522",
+  appId: "1:229837698522:web:ddb9db6faf98f83d027ce3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
