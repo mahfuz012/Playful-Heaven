7d9e4f76b46c4408faef751f3c722ec0a668da05
mahfuz012 <mahfuzrn012@gmail.com>
2023-05-20 20:06:48 +0600

    react rating add

--- src/ComponentAll/DetailsToys.jsx
+++ src/ComponentAll/DetailsToys.jsx
@@ -18,20 +18,34 @@ const DetailsToys = () => {
         <div className='container sm:my-20'>
 
 <div className="card lg:card-side bg-base-100 border shadow-xl">
+
 <img src={image_url} className='border w-2/4' alt="Album"/>
 
-  <div className="card-body">
-    <h2 className="card-title font-bold">Toy Name : <span className='font-bold text-2xl text-purple-700'>{Name}</span></h2>
-    
-  <p className = "font-bold text-xl">Price : <span className='text-purple-700'>{Price}$</span></p>
-  
+
+    <div className='px-5 py-8 sm:flex sm:flex-col '>
+    <p className="font-bold text-2xl my-2">Toy Name : <span className='font-bold text-xl text-purple-700'>{Name}</span></p>
+    <p></p>
+  <p className = "font-bold text-2xl my-2">Price : <span className='text-purple-700'>{Price}$</span></p>
+<p className='font-bold'>Rating: {Rating}</p>
+<p className='font-bold mb-2'>Quantity: {Available_quantity}</p>
+
+
+
+  <p className = "font-semibold ">Seller Name :  <span className='text-purple-700'>{seller_name}</span></p>
+  <p className = "font-semibold ">Seller Email : <span className='text-purple-700'>{seller_email}</span></p>
+<p className='font-bold  my-5'>Details : <span className='font-normal'>{Detail_description}</span> </p>
+
+
+
+    </div>
+
 
 
 
 
 
    
-  </div>
+
 </div>
         </div>
     );
